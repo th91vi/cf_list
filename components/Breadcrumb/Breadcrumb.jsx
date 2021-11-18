e1607bfb1781fd303f1367270bd445d057baeda7
Thiago Vieira <th91vi@gmail.com>
2021-11-18 17:30:08 -0300

    refactor: Breadcrumb style refactoring

--- components/Breadcrumb/Breadcrumb.jsx
+++ components/Breadcrumb/Breadcrumb.jsx
@@ -1,21 +1,39 @@
-import { Breadcrumb, Stack } from "react-bootstrap";
+import Image from "next/image";
+import { Breadcrumb, Row, Col, Stack } from "react-bootstrap";
 import { FaHome } from "react-icons/fa";
 
 export function HeaderBreadcrumb() {
   return (
-    <Breadcrumb>
-      <Breadcrumb.Item>
-        <Stack direction="horizontal">
-          <span style={{ marginRight: "8px" }}>
-            <FaHome className="mb-1" />
-          </span>
-          <span>Home</span>
-        </Stack>
-      </Breadcrumb.Item>
-      <Breadcrumb.Item>Medicamentos</Breadcrumb.Item>
-      <Breadcrumb.Item>Disfunção Erétil</Breadcrumb.Item>
-      <Breadcrumb.Item>Viagra</Breadcrumb.Item>
-      <Breadcrumb.Item active>100mg 4 Comprimido(s)</Breadcrumb.Item>
-    </Breadcrumb>
+    <>
+      <Row>
+        <Col md={8}>
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Stack direction="horizontal">
+                <span style={{ marginRight: "8px" }}>
+                  <FaHome className="mb-1" />
+                </span>
+                <span>Home</span>
+              </Stack>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>Medicamentos</Breadcrumb.Item>
+            <Breadcrumb.Item>Disfunção Erétil</Breadcrumb.Item>
+            <Breadcrumb.Item>Viagra</Breadcrumb.Item>
+            <Breadcrumb.Item active>100mg 4 Comprimido(s)</Breadcrumb.Item>
+          </Breadcrumb>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <pre style={{ marginBottom: "0px" }}>PUBLICIDADE</pre>
+          <Image
+            src={"/banner-pre-bf-topo-dektop.png"}
+            alt={"Economize ainda mais! Black Friday CliqueFarma"}
+            width={720}
+            height={90}
+          />
+        </Col>
+      </Row>
+    </>
   );
 }
